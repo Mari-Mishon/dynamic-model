cbd2cfba43d3918d28b362b3b213faccfbd99b03
Mari-Mishon <mashenka-777@bk.ru>
2018-03-28 00:31:58 +0700

    Фильтр+оценка качества системы

--- mari_1_lab.m
+++ mari_1_lab.m
@@ -13,17 +13,19 @@ tline = fgetl(fid);
 [T,~] =str2num(tline);
 
 X= X';
+X0 = X;
 U= U';
 N= N';
 Y= Y';
+Y0=Y;
 
-A = randn(size(X,1),size(X,1));
-% A=[[-0.03333333 0.1 0.01666667 0.2 0.26666667 0.02 ]
-% [ 0.33333333 0.03333333 -0.33333333 0.33333333 0.06666667 0.33333333]
-% [-0.33333333 -0.33333333 -0.2 0.33333333 0.1 0.16666667]
-% [ 0.02333333 -0.03 0.33333333 0.06666667 -0.16666667 -0.26666667]
-% [ 0.33333333 -0.33333333 0.23333333 0.3 -0.3 -0.1 ]
-% [ 0.33333333 0.33333333 0.26666667 0.16666667 0.13333333 0.06666667]];
+%A = randn(size(X,1),size(X,1));
+A=[[-0.03333333 0.1 0.01666667 0.2 0.26666667 0.02 ]
+[ 0.33333333 0.03333333 -0.33333333 0.33333333 0.06666667 0.33333333]
+[-0.33333333 -0.33333333 -0.2 0.33333333 0.1 0.16666667]
+[ 0.02333333 -0.03 0.33333333 0.06666667 -0.16666667 -0.26666667]
+[ 0.33333333 -0.33333333 0.23333333 0.3 -0.3 -0.1 ]
+[ 0.33333333 0.33333333 0.26666667 0.16666667 0.13333333 0.06666667]];
 
 
 B = randn(size(X,1),size(U,1));
@@ -33,6 +35,8 @@ D = randn(size(Y,1),size(U,1));
 F = randn(size(Y,1),size(N,1));
 
 outputs=[];
+states=[];
+outputs0=[];
 
 t=0;
 while t < T
@@ -40,6 +44,7 @@ while t < T
     Y = C*X + D*U + F*N;
     t=t+1;
     outputs=[outputs,Y];
+    states=[states,X];
 end
 
 dlmwrite('C:\Users\Mary\Desktop\dynamic-model-master\output.txt', Y);
@@ -67,4 +72,42 @@ end
 time=0:(T-1); 
 plot(time,outputs);
 
-    
+window = 3;
+alpha=1/window;
+
+length_y = length(outputs);
+MMAy = zeros(1, length_y);
+MMAy(1) = outputs(1);
+for i=2:length_y
+     MMAy(i) = alpha*outputs(i) + (1-alpha)*MMAy(i-1);
+end
+
+hold on;
+plot(MMAy);
+
+tpp=0;
+overshoot=0;
+final_value=0;
+
+if (stability==0) 
+t=0;
+    while t < 10000
+        X0 = A*X0 + B*U + E*N; 
+        Y0 = C*X0 + D*U + F*N;
+        t=t+1;
+        outputs0=[outputs0,Y0];
+    end
+final_value=outputs0(end);
+overshoot=100*(max(outputs0)-final_value)/final_value;
+
+for i=1:length(outputs0)
+    if abs(outputs0(i)-final_value)/final_value > 0.05
+        tpp=i;
+    end    
+end
+
+end 
+
+ disp(overshoot); 
+ disp(tpp);
+ disp(final_value);
